501309773da6b72c9878b5585e04aa35aeb5317a
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-12-09 20:51:48 +0900

    Initial commit.

---
+++ Unity/Assets/PluginTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class PluginTest : MonoBehaviour
+{
+    public int iteration = 50000;
+
+    int[] temp;
+
+    [DllImport ("SamplePlugin")]
+    static extern void TestFunction (int[] array, int length);
+
+    void Start ()
+    {
+        temp = new int[1024 * 2];
+    }
+
+    void Update ()
+    {
+        for (var i = 0; i < iteration; i++)
+        {
+            TestFunction (temp, temp.Length);
+        }
+    }
+}
