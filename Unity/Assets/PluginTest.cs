511d7417ccb6bd1915de2565cfaee78bb3b6ec95
Keijiro Takahashi <keijiro@gmail.com>
2014-05-15 18:42:34 +0900

    Added writeVector3 test.

--- Unity/Assets/PluginTest.cs
+++ Unity/Assets/PluginTest.cs
@@ -4,28 +4,66 @@ using System.Runtime.InteropServices;
 
 public class PluginTest : MonoBehaviour
 {
-    public int iteration = 50000;
+    public int iteration = 1000;
 
-    int[] temp;
+    int[] intArray;
+    Vector3[] vector3Array;
+    GCHandle vector3ArrayHandle;
 
 #if UNITY_EDITOR || UNITY_STANDALONE_OSX
+
     [DllImport ("SamplePlugin")]
-    static extern void TestFunction (int[] array, int length);
+    static extern void WriteIntArray (int[] array, int length);
+
+    [DllImport ("SamplePlugin")]
+    static extern void WriteVector3Array (System.IntPtr array, int length);
+
 #elif UNITY_IPHONE
-	[DllImport ("__Internal")]
-	static extern void TestFunction (int[] array, int length);
+
+    [DllImport ("__Internal")]
+    static extern void WriteIntArray (int[] array, int length);
+
+    [DllImport ("__Internal")]
+    static extern void WriteVector3Array (System.IntPtr array, int length);
+
 #endif
 
     void Start ()
     {
-        temp = new int[1024 * 2];
+        intArray = new int [1024 * 3];
+        vector3Array = new Vector3 [1024];
+        vector3ArrayHandle = GCHandle.Alloc (vector3Array, GCHandleType.Pinned);
+    }
+
+    void OnDisable ()
+    {
+        if (vector3ArrayHandle.IsAllocated) vector3ArrayHandle.Free ();
     }
 
     void Update ()
     {
         for (var i = 0; i < iteration; i++)
         {
-            TestFunction (temp, temp.Length);
+            WriteIntArray (intArray, intArray.Length);
+            WriteVector3Array (vector3ArrayHandle.AddrOfPinnedObject(), vector3Array.Length);
+        }
+
+        for (var i = 0; i < intArray.Length; i++)
+        {
+            if (intArray[i] != i)
+            {
+                Debug.LogError("Array contents differ (intArray)");
+                Debug.Break();
+            }
+        }
+
+        for (var i = 0; i < vector3Array.Length; i++)
+        {
+            if (vector3Array[i] != new Vector3(i * 3, i * 3 + 1, i * 3 + 2))
+            {
+                Debug.LogError("Array contents differ (vector3Array)");
+                Debug.Break();
+            }
         }
     }
 }
--- Unity/Assets/Plugins/iOS/EntryPoint.c
+++ Unity/Assets/Plugins/iOS/EntryPoint.c
@@ -1,6 +1,14 @@
-void TestFunction(int *array, int length)
+void WriteIntArray(int *array, int length)
 {
     for (int i = 0; i < length; i++)
+    {
+        array[i] = i;
+    }
+}
+
+void WriteVector3Array(float *array, int length)
+{
+    for (int i = 0; i < length * 3; i++)
     {
         array[i] = i;
     }
